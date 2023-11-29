236c8f8aadc65795df236b749206c68e77b615a9
Sky1sBloo <ultimate8gaming@gmail.com>
2023-11-29 15:25:41 +0800

    Added my configuration

--- .ccls-cache/@home@jdp@repos@dmenu/config.def.h
+++ .ccls-cache/@home@jdp@repos@dmenu/config.def.h
@@ -11,10 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#282828" },
-	[SchemeSel] = { "#eeeeee", "#504945" },
-	[SchemeSelHighlight] = { "#ffc978", "#504945" },
-	[SchemeNormHighlight] = { "#ffc978", "#282828" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
